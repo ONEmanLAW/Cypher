69cce9b638c47689e119a5e655e2794bebd84ceb
Hugo Culot <hugoculot.dev@gmail.com>
2026-05-18 14:03:42 +0200

    Added structure to app and added a ui component for icons (BaseIncons.vue).

---
+++ src/components/ui/BaseIcons.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  icon: { type: [Object, Function], required: true },
+  size: { type: Number, default: 24 },
+})
+</script>
+
+<template>
+  <component :is="icon" :size="size" :stroke-width="1.5" />
+</template>
