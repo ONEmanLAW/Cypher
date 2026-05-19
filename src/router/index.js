858bbae41d049a325d11ace96075318d0ea47db9
Hugo Culot <hugoculot.dev@gmail.com>
2026-05-19 23:48:51 +0200

    Added exo1 to index.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import Exo01View from '@/views/Exo01View.vue'
 import Exo02View from '@/views/Exo02View.vue'
 import Exo03View from '@/views/Exo03View.vue'
 import Exo04View from '@/views/Exo04View.vue'
@@ -10,6 +11,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: HomeView },
+    { path: '/exo-01', name: 'exo01', component: Exo01View },
     { path: '/exo-02', name: 'exo02', component: Exo02View },
     { path: '/exo-03', name: 'exo03', component: Exo03View },
     { path: '/exo-04', name: 'exo04', component: Exo04View },
